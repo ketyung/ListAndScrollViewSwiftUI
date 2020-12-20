bf3aba161eb3f82274a47887a02453c3ed1c8f1d
Christopher Chee <ketyung@techchee.com>
2020-12-20 10:12:55 +0800

    Initial Commit

---
+++ Shared/ListAndScriollViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAndScriollViewSwiftUIApp.swift
+//  Shared
+//
+//  Created by Chee Ket Yung on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ListAndScriollViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
